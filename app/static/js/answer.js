c426d871f6adfe94cfe4d6064825d1bad28d9067
josephtey <jjttey@gmail.com>
2017-06-02 12:47:05 +1000

    string similarity

--- app/static/js/answer.js
+++ app/static/js/answer.js
@@ -1,6 +1,39 @@
 /**
  * Created by raeste on 09.03.17.
  */
+function ld(a, b) {
+    if(a.length == 0) return b.length; 
+    if(b.length == 0) return a.length; 
+  
+    var matrix = [];
+  
+    // increment along the first column of each row
+    var i;
+    for(i = 0; i <= b.length; i++){
+      matrix[i] = [i];
+    }
+  
+    // increment each column in the first row
+    var j;
+    for(j = 0; j <= a.length; j++){
+      matrix[0][j] = j;
+    }
+  
+    // Fill in the rest of the matrix
+    for(i = 1; i <= b.length; i++){
+      for(j = 1; j <= a.length; j++){
+        if(b.charAt(i-1) == a.charAt(j-1)){
+          matrix[i][j] = matrix[i-1][j-1];
+        } else {
+          matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+                                  Math.min(matrix[i][j-1] + 1, // insertion
+                                           matrix[i-1][j] + 1)); // deletion
+        }
+      }
+    }
+  
+    return matrix[b.length][a.length];
+  }
 
 function showAnswer() {
     stopTimer()
@@ -19,7 +52,7 @@ function showAnswer() {
     user_response = response.value.replace(/\s/g,'');
 
     if (answer.style.display != 'block'){
-      if (ans.toUpperCase() === user_response.toUpperCase()) {
+      if (ld(ans.toUpperCase(), user_response.toUpperCase()) < 2) {
           right_answer.style.display = 'block';
           answer_txt.className += ' green';
           localStorage.setItem('outcome', true)
