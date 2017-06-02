5ac3d02635a8b168658e1ca952e0dda40530cf48
josephtey <jjttey@gmail.com>
2017-06-02 11:12:58 +1000

    updated

--- app/static/js/answer.js
+++ app/static/js/answer.js
@@ -8,30 +8,36 @@ function showAnswer() {
     var wrong_answer = document.getElementById('wrong_answer');
     var right_link = document.getElementById('right_link');
     var wrong_link = document.getElementById('wrong_link');
-    var buttons = document.getElementById('buttons')
+    var buttons = document.getElementById('buttons');
 
     var response = document.getElementById('response');
-    var answer = document.getElementById('answer')
-    var answer_txt = document.getElementById('answer_text')
+    var answer = document.getElementById('answer');
+    var answer_txt = document.getElementById('answer_text');
     var ans = answer_txt.textContent;
+
     ans = ans.replace(/\s/g,'');
     user_response = response.value.replace(/\s/g,'');
 
-    
-    console.log(ans + ", " +user_response);
-    if (ans.toUpperCase() === user_response.toUpperCase()) {
-        right_answer.style.display = 'block';
-        answer_txt.className += ' green';
-    } else {
-        wrong_answer.style.display = 'block';
-        answer_txt.className += ' red';
+    if (answer.style.display != 'block'){
+      if (ans.toUpperCase() === user_response.toUpperCase()) {
+          right_answer.style.display = 'block';
+          answer_txt.className += ' green';
+          localStorage.setItem('outcome', true)
+      } else {
+          wrong_answer.style.display = 'block';
+          answer_txt.className += ' red';
+          localStorage.setItem('outcome', false)
+      }
     }
 
     if (answer.style.display == 'block') {
-        if (ans.toUpperCase() === user_response.toUpperCase()) {
+        if (localStorage.getItem('outcome') == 'true') {
+            console.log('correct');
             right_link.click();
         } else {
+            console.log('wrong')
             wrong_link.click();
+            
         }
     }
     answer.style.display = 'block';
@@ -48,6 +54,7 @@ function stopTimer(){
 function focusTextBox(){
     document.getElementById('response').focus();
 }
+
 function detect(event) {
     var char = event.which || event.keyCode;
     if (char == 13) {
